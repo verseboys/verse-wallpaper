6977e3fc6d1606e132a4b8a4c7e8cc2990be21da
yxq <verseyang@gmail.com>
2024-10-05 18:14:10 +0800

    版本更新至1.0.3，并添加新功能
    - 将版本号从1.0.2更新至1.0.3
    - 添加了设置锁屏壁纸的功能
    - 新增删除锁屏壁纸的功能
    - 优化了代码格式，调整了部分导入语句的格式

--- source/windows.js
+++ source/windows.js
@@ -1,34 +1,40 @@
-import {promisify} from 'node:util';
-import childProcess from 'node:child_process';
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
+import { promisify } from "node:util";
+import childProcess from "node:child_process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const execFile = promisify(childProcess.execFile);
 
 // Binary source → https://github.com/sindresorhus/windows-wallpaper
-const binary = path.join(__dirname, 'windows-wallpaper-x86-64.exe');
-
+const binary = path.join(__dirname, "windows-wallpaper-x86-64.exe");
+const lockscreen_wallpaper_binary = path.join(
+	__dirname,
+	"lockscreen_wallpaper.exe"
+);
 export async function getWallpaper() {
-	const arguments_ = [
-		'get',
-	];
+	const arguments_ = ["get"];
 
-	const {stdout} = await execFile(binary, arguments_);
+	const { stdout } = await execFile(binary, arguments_);
 	return stdout.trim();
 }
 
-export async function setWallpaper(imagePath, {scale = 'fill'} = {}) {
-	if (typeof imagePath !== 'string') {
-		throw new TypeError('Expected a string');
+export async function setWallpaper(imagePath, { scale = "fill" } = {}) {
+	if (typeof imagePath !== "string") {
+		throw new TypeError("Expected a string");
 	}
 
-	const arguments_ = [
-		'set',
-		path.resolve(imagePath),
-		'--scale',
-		scale,
-	];
+	const arguments_ = ["set", path.resolve(imagePath), "--scale", scale];
 
 	await execFile(binary, arguments_);
+}
+
+export async function setLockscreenWallpaper(imagePath) {
+	const arguments_ = ["set", path.resolve(imagePath)];
+	await execFile(lockscreen_wallpaper_binary, arguments_);
+}
+
+export async function removeLockscreenWallpaper() {
+	const arguments_ = ["remove"];
+	await execFile(lockscreen_wallpaper_binary, arguments_);
 }
